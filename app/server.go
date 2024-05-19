9aacd3412c773b01fef80f51bf52250f8fb57181
Daniel Jodłoś <danjod40@gmail.com>
2024-05-19 08:49:56 +0200

    point to correct header name in stage 10

--- app/server.go
+++ app/server.go
@@ -190,7 +190,7 @@ func handleConnection(conn net.Conn) {
 		}
 	}
 
-	if encoding, ok := request.headers.get("Content-Encoding"); ok && isSupportedEncoding(encoding) {
+	if encoding, ok := request.headers.get("Accept-Encoding"); ok && isSupportedEncoding(encoding) {
 		response.setContentEncoding(encoding)
 	}
 
